780bacee83be4da8b39f4b9117ec10ff41284ed0
kajgrant <kajgrant@gmail.com>
2024-05-31 10:53:46 -0700

    update tests

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,8 +1,8 @@
 // https://on.cypress.io/api
 
-describe('My First Test', () => {
+describe('Page navigation', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.contains('h1', 'Stock Tracker')
   })
 })
--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -5,7 +5,7 @@ import HelloWorld from '../HelloWorld.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(HelloWorld, { props: { msg: 'Stock Tracker' } })
+    expect(wrapper.text()).toContain('Stock Tracker')
   })
 })
