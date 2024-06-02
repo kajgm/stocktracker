1a42f7063c9614bfcda1e230ed321e5eaa749d50
kajgrant <kajgrant@gmail.com>
2024-06-01 20:24:31 -0700

    removal of cypress tests for docker
    cypress does not support the raspberry pi arm
    achitecture, will need to use another
    framework

--- cypress/e2e/Ticker.cy.ts
+++
@@ -1,8 +0,0 @@
-// https://on.cypress.io/api
-
-describe('Page navigation', () => {
-  it('visits the app root url', () => {
-    cy.visit('/');
-    cy.get('[data-cy="ticker"]').contains('(Coinbase)');
-  });
-});
