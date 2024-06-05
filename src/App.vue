6991e603706103b7f6188c88817206355c904460
kajgrant <kajgrant@gmail.com>
2024-06-05 12:41:58 -0700

    enable stock api and minor css fix

--- src/App.vue
+++ src/App.vue
@@ -93,93 +93,27 @@ function websocketConnect(endpoint: string, subMsg: any, msgFn: (e: MessageEvent
 }
 
 function restApiPoll() {
-  // axios
-  //   .get(stockURL + stockTickers.toString() + '?apikey=' + import.meta.env.VITE_VUE_APP_FMP_KEY)
-  //   .then((res) => {
-  //     for (let i = 0; i < res.data.length; i++) {
-  //     const prevRes = tickerResponse.get(res.data[i].symbol) || defaultTicker;
-  //       const stock = {
-  //         id: res.data[i].symbol,
-  //         curPrice: res.data[i].price,
-  //         volume: res.data[i].volume,
-  //         prevPrice: prevRes.curPrice,
-  //         dirFilter: priceDirection(prevRes.dirFilter, prevRes.curPrice, prevRes.prevPrice)
-  //       } as TickerType;
-  //       tickerResponse.set(res.data[i].symbol, stock);
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //   })
-  //   .finally(() => {
-  //     // need to add updates to the connecting status here
-  //   });
+  axios
+    .get(stockURL + stockTickers.toString() + '?apikey=' + import.meta.env.VITE_VUE_APP_FMP_KEY)
+    .then((res) => {
+      for (let i = 0; i < res.data.length; i++) {
+        const prevRes = tickerResponse.get(res.data[i].symbol) || defaultTicker;
+        const stock = {
+          id: res.data[i].symbol,
+          curPrice: res.data[i].price.toLocaleString('en', { minimumFractionDigits: 2 }),
+          volume: res.data[i].volume,
+          prevPrice: prevRes.curPrice,
+          dirFilter: priceDirection(prevRes.dirFilter, prevRes.curPrice, prevRes.prevPrice),
+          status: 'connected'
+        } as TickerType;
+        tickerResponse.set(res.data[i].symbol, stock);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-  const res = [
-    {
-      symbol: 'AAPL',
-      name: 'Apple Inc.',
-      price: 195.9797,
-      changesPercentage: 0.8385,
-      change: 1.6297,
-      dayLow: 194.88,
-      dayHigh: 196.9,
-      yearHigh: 199.62,
-      yearLow: 164.08,
-      marketCap: 3005172317770,
-      priceAvg50: 178.0664,
-      priceAvg200: 181.4276,
-      exchange: 'NASDAQ',
-      volume: '28673753',
-      avgVolume: 61275561,
-      open: 195.4,
-      previousClose: 194.35,
-      eps: 6.43,
-      pe: 30.48,
-      earningsAnnouncement: '2024-08-01T10:59:00.000+0000',
-      sharesOutstanding: 15334100000,
-      timestamp: 1717608650
-    },
-    {
-      symbol: 'MSFT',
-      name: 'Microsoft Corporation',
-      price: '422.86',
-      changesPercentage: 1.6319,
-      change: 6.79,
-      dayLow: 416.3,
-      dayHigh: 423.35,
-      yearHigh: 433.6,
-      yearLow: 309.45,
-      marketCap: 3142826606600,
-      priceAvg50: 415.8776,
-      priceAvg200: 379.92606,
-      exchange: 'NASDAQ',
-      volume: '7661534',
-      avgVolume: 19836049,
-      open: 417.81,
-      previousClose: 416.07,
-      eps: 11.53,
-      pe: 36.67,
-      earningsAnnouncement: '2024-07-23T10:59:00.000+0000',
-      sharesOutstanding: 7432310000,
-      timestamp: 1717608651
-    }
-  ];
-
-  for (let i = 0; i < res.length; i++) {
-    const prevRes = tickerResponse.get(res[i].symbol) || defaultTicker;
-    const stock = {
-      id: res[i].symbol,
-      curPrice: res[i].price.toLocaleString('en', { minimumFractionDigits: 2 }),
-      volume: res[i].volume,
-      prevPrice: prevRes.curPrice,
-      dirFilter: priceDirection(prevRes.dirFilter, prevRes.curPrice, prevRes.prevPrice),
-      status: 'connected'
-    } as TickerType;
-    tickerResponse.set(res[i].symbol, stock);
-  }
-
-  console.log('called api!');
+  console.log('Called FMP api');
   setTimeout(restApiPoll, API_TIMEOUT);
 }
 
