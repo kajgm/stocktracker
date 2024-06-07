81df70360b307cf724801b0139dee9ae61c3e1ae
kajgrant <kajgrant@gmail.com>
2024-06-07 11:30:54 -0700

    fix tests to use pinia

--- src/components/__tests__/App.spec.ts
+++ src/components/__tests__/App.spec.ts
@@ -1,8 +1,13 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { mount } from '@vue/test-utils';
+import { setActivePinia, createPinia } from 'pinia';
 import App from '@/App.vue';
 
 describe('App', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
   it('renders properly', () => {
     const wrapper = mount(App);
     expect(wrapper.text()).toContain('Connecting...');
