e1c156c337af12cbdee8ea0de4a2c97c0c038bb2
kajgrant <kajgrant@gmail.com>
2024-06-14 18:21:12 -0700

    update client config files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
 import EnvironmentPlugin from 'vite-plugin-environment';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    EnvironmentPlugin(['FMP_KEY', 'STOCK_TICKERS', 'CRYPTO_TICKERS'])
-    // EnvironmentPlugin({
-    //   FMP_KEY: 'undefined',
-    //   STOCK_TICKERS: 'MSFT,AAPL,NVDA,AMD,INTC,AMZN',
-    //   CRYPTO_TICKERS: 'ETH-USD,BTC-USD'
-    // })
+    EnvironmentPlugin({
+      FMP_KEY: 'undefined',
+      STOCK_TICKERS: 'MSFT,AAPL,NVDA,AMD,INTC,AMZN',
+      CRYPTO_TICKERS: 'ETH-USD,BTC-USD'
+    })
   ],
   resolve: {
     alias: {
